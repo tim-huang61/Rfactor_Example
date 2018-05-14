4da6e44b67f011b6d8219b45ab0c394e1e3770d0
tim <tim.huang61@gmail.com>
2018-05-14 13:30:35 +0800

    Add project files.

---
+++ Rfactor_Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rfactor_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new HelloWorld();
+
+            Console.Read();
+        }
+    }
+
+    public class HelloWorld
+    {
+    }
+}
